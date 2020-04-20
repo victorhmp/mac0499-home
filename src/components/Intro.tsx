a24a37901b9e1de1fd8977d77e48f8460f7a7587
Victor Hugo M. Pinto <victor2142@gmail.com>
2020-04-20 00:31:59 -0300

    Update Intro text

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,8 +3,13 @@ import React from 'react'
 const Intro = () => {
   return (
     <div>
-      Essa é a home de todo o trabalho que será desenvolvido por mim com
-      orientação do professor Alfredo Goldman
+      Essa página será utilizada para o acompanhamento do Trabalho de Conclusão
+      de Curso{' '}
+      <strong>
+        &quot;O papel da documentação no desenvolvimento de softwares
+        open-source de sucesso e como criar uma cultura que a suporte&quot;
+      </strong>
+      , orientado pelo professor Alfredo Goldman.
     </div>
   )
 }
