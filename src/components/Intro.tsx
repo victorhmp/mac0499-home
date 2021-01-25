444c7ae4613d2c18dc64fb44712b8de64208ffcd
victorhmp <victor2142@gmail.com>
2021-01-25 09:57:10 -0300

    Add project's abstract

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,13 +3,24 @@ import React from 'react'
 const Intro = () => {
   return (
     <div>
-      Essa página será utilizada para o acompanhamento do Trabalho de Conclusão
-      de Curso{' '}
-      <strong>
-        &quot;O papel da documentação no desenvolvimento de softwares
-        open-source de sucesso e como criar uma cultura que a suporte&quot;
-      </strong>
-      , orientado pelo professor Alfredo Goldman.
+      <h2>
+        O papel da documentação no desenvolvimento de softwares open source: Uma
+        análise e um estudo de caso
+      </h2>
+      <h3>Resumo</h3>A documentação de um software é valiosa tanto para a sua
+      manutenção quanto evolução. Ela é vista como a principal forma de
+      comunicação de informações sobre um software. Neste trabalho, buscamos
+      entender melhor como a documentação é percebida por desenvolvedores, quais
+      os problemas mais comuns encontrados, e como projetos <em>open source</em>{' '}
+      de sucesso lidam com suas documentações. Após um levantamento
+      bibliográfico sobre o tema e estudo de alguns dos projetos{' '}
+      <em>open source</em> mais proeminentes do GitHub, aplicamos o que
+      aprendemos em um projeto de software real. Implementamos um sistema para
+      apoiar a documentação do VTEX IO, incluindo um novo portal de
+      documentação. Junto à implementação deste sistema, foram implementadas
+      mudanças culturais com um time dentro do VTEX IO. Tais mudanças tinham
+      como objetivo trazer a documentação para o fluxo de desenvolvimento,
+      elevando sua importância.
     </div>
   )
 }
